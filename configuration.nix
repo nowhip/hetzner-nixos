ca1b2a3e8ab694150100e07953c28400762159e0
selim <selimyag.eser@gmail.com>
2026-04-29 18:11:07 +0200

    formatted Flake and Configuration

--- configuration.nix
+++ configuration.nix
@@ -1,84 +1,73 @@
 { pkgs, ... }:
- 
- {
-   nix.settings = {
-     experimental-features = "nix-command flakes";
-   };
-   
-   environment.systemPackages = [
-     pkgs.git
-     pkgs.nixfmt
-   ];
-   
-   fileSystems."/" = {
-     device = "/dev/disk/by-label/nixos";
-     fsType = "ext4";
-   };
-   fileSystems."/boot" = {
-     device = "/dev/disk/by-label/boot";
-     fsType = "ext4";
-   };
-   swapDevices = [
-     {
-       device = "/dev/disk/by-label/swap";
-     }
-   ];
-   
-   time.timeZone = "Europe/Berlin";
-   i18n.defaultLocale = "en_US.UTF-8";
-   console.keyMap = "de";
-   
-   boot.loader.grub.enable = true;
-   boot.loader.grub.device = "/dev/sda";
-   boot.initrd.availableKernelModules = [ "ahci" "xhci_pci" "virtio_pci" "virtio_scsi" "sd_mod" "sr_mod" "ext4" ];
-   
-   users.users = {
-     root.hashedPassword = "!"; # Disable root login
-     selim = {
-       isNormalUser = true;
-       extraGroups = [ "wheel" ];
-       openssh.authorizedKeys.keys = [
-         "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOoPbdeg6m8b7fWa6Og/yNespDkC69mj0frS1pfk0SxP selim"
-       ];
-     };
-     daniil = {
-       isNormalUser = true;
-       openssh.authorizedKeys.keys = [
-         "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIMsJHmi82YhxJd4f7Cmh5O0k5WkakyOn7o8b5JXqA4xM daniil"
-       ];
-       packages = with pkgs; [
-         python3
-         asciiquarium
-       ];
-     };
-     boris = {
-       isNormalUser = true;
-       openssh.authorizedKeys.keys = [
-         "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOhMNIBDVF0CY6eoCOQatiUzqKNUHr8vFJUaOqwRVCOf boris"
-       ];
-     };
-     erkin = {
-       isNormalUser = true;
-       openssh.authorizedKeys.keys = [
-         "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIL4DEcXxqjv6irSJMhkaovNbQ+swy8UHmb2l6dakp2nw erkin"
-       ];
-     };
-   };
 
-   security.sudo.wheelNeedsPassword = false;
+{
+  nix.settings = { experimental-features = "nix-command flakes"; };
 
-   services.openssh = {
-     enable = true;
-     settings = {
-       PermitRootLogin = "no";
-       PasswordAuthentication = false;
-       KbdInteractiveAuthentication = false;
-     };
-   };
-   
-   networking.hostName = "selims-server";
+  environment.systemPackages = [ pkgs.git pkgs.nixfmt ];
 
-   networking.firewall.allowedTCPPorts = [ 22 ];
+  fileSystems."/" = {
+    device = "/dev/disk/by-label/nixos";
+    fsType = "ext4";
+  };
+  fileSystems."/boot" = {
+    device = "/dev/disk/by-label/boot";
+    fsType = "ext4";
+  };
+  swapDevices = [{ device = "/dev/disk/by-label/swap"; }];
 
-   system.stateVersion = "24.11";
- }
+  time.timeZone = "Europe/Berlin";
+  i18n.defaultLocale = "en_US.UTF-8";
+  console.keyMap = "de";
+
+  boot.loader.grub.enable = true;
+  boot.loader.grub.device = "/dev/sda";
+  boot.initrd.availableKernelModules =
+    [ "ahci" "xhci_pci" "virtio_pci" "virtio_scsi" "sd_mod" "sr_mod" "ext4" ];
+
+  users.users = {
+    root.hashedPassword = "!"; # Disable root login
+    selim = {
+      isNormalUser = true;
+      extraGroups = [ "wheel" ];
+      openssh.authorizedKeys.keys = [
+        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOoPbdeg6m8b7fWa6Og/yNespDkC69mj0frS1pfk0SxP selim"
+      ];
+    };
+    daniil = {
+      isNormalUser = true;
+      openssh.authorizedKeys.keys = [
+        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIMsJHmi82YhxJd4f7Cmh5O0k5WkakyOn7o8b5JXqA4xM daniil"
+      ];
+      packages = with pkgs; [ python3 asciiquarium ];
+    };
+    boris = {
+      isNormalUser = true;
+      openssh.authorizedKeys.keys = [
+        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOhMNIBDVF0CY6eoCOQatiUzqKNUHr8vFJUaOqwRVCOf boris"
+      ];
+    };
+    erkin = {
+      isNormalUser = true;
+      openssh.authorizedKeys.keys = [
+        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIL4DEcXxqjv6irSJMhkaovNbQ+swy8UHmb2l6dakp2nw erkin"
+      ];
+    };
+  };
+
+  security.sudo.wheelNeedsPassword = false;
+
+  services.openssh = {
+    enable = true;
+    settings = {
+      PermitRootLogin = "no";
+      PasswordAuthentication = false;
+      KbdInteractiveAuthentication = false;
+    };
+  };
+
+  networking.hostName = "selims-server";
+
+  networking.firewall.allowedTCPPorts = [ 22 ];
+
+  system.stateVersion = "24.11";
+}
--- flake.nix
+++ flake.nix
@@ -1,21 +1,19 @@
 {
-   inputs = {
-     nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
-     vscode-server.url = "github:nix-community/nixos-vscode-server";
-   };
- 
-   outputs = { nixpkgs, vscode-server, ... }: {
-     nixosConfigurations = {
-       selims-server = nixpkgs.lib.nixosSystem {
-         system = "x86_64-linux";
-         modules = [
-           ./configuration.nix
-           vscode-server.nixosModules.default
-           {
-             services.vscode-server.enable = true;
-           }
-         ];
-       };
-     };
-   };
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
+    vscode-server.url = "github:nix-community/nixos-vscode-server";
+  };
+
+  outputs = { nixpkgs, vscode-server, ... }: {
+    nixosConfigurations = {
+      selims-server = nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
+        modules = [
+          ./configuration.nix
+          vscode-server.nixosModules.default
+          { services.vscode-server.enable = true; }
+        ];
+      };
+    };
+  };
 }
